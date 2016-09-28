f5c8b33a1bc4e0c1551484b8579cd00c41121ed7
gaoce <contact.gaoce@gmail.com>
2016-09-28 16:21:53 -0400

    Remove safeguard of View (the default error msg is informative enough)

--- R/inspection.R
+++ R/inspection.R
@@ -6,10 +6,7 @@ view_name = function() {
     context = rstudioapi::getSourceEditorContext()
     if (length(context$selection) == 1 & context$selection[[1]]$text != '') {
         obj_name = context$selection[[1]]$text
-        obj = get(obj_name, envir = globalenv())
-        if (is.data.frame(obj)) {
-            expr_str = sprintf('View(%s)', obj_name)
-            eval(parse(text = expr_str), envir = globalenv())
-        }
+        expr_str = sprintf('View(%s)', obj_name)
+        eval(parse(text = expr_str), envir = globalenv())
     }
 }
