5e2f5fc0d4f97cda4f0534b66c15926d9fc0b045
gaoce <contact.gaoce@gmail.com>
2016-10-07 11:19:42 -0400

    Use getActiveDocumentContext so view_name works in both editor and console, and add some more comments

--- R/inspection.R
+++ R/inspection.R
@@ -4,11 +4,13 @@
 #' @import stringr
 #' @export
 view_name = function() {
-    context = rstudioapi::getSourceEditorContext()
+    # Get context from editor or console
+    context = rstudioapi::getActiveDocumentContext()
 
     # Multiple selection, abort
     if (length(context$selection) > 1) return()
 
+    # Start and end positions of selection[[1]]
     pos_frt = context$selection[[1]]$range$start
     pos_end = context$selection[[1]]$range$end
 
@@ -20,6 +22,7 @@ view_name = function() {
     # Get location of valid variable names
     locs = stringr::str_locate_all(line, '[a-zA-Z][a-zA-Z0-9_\\.]*')[[1]]
 
+    # Test relative position between cursor and tokens
     pos = which(locs[, 1] <= pos_frt[2] & locs[, 1] <= pos_end[2] &
                 locs[, 2] >= (pos_frt[2] - 1) & locs[, 2] >= (pos_end[2] - 1))
 
@@ -32,6 +35,10 @@ view_name = function() {
     expr_str = sprintf('View(%s)', obj_name)
 
     # Evaluation and capture the error
+    # Note 1: We must view the variable directly, see "Using the Data Viewer"
+    #   on RStudio Support
+    # Note 2: RStudio hajack the utils::View and cat() the error instead of
+    #   "throw" it, so we need to capture.output() to become fully silent
     capture.output(
         try(eval(parse(text = expr_str), envir = globalenv()), silent = T)
     )
